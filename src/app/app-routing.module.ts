65385eac517a19a32f5e5acc37e8ab97463763a9
marciosmelo <marciomelo@gmail.com>
2018-02-27 22:13:54 -0300

    Adding the Dashboard service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClonesComponent }      from './clones/clones.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'clones', component: ClonesComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'clones', component: ClonesComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
