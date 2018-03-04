ab4d301265d129810869a33a6ed94558cd1fd95c
Luis Ferreira <luisrbferreira@gmail.com>
2018-03-04 12:46:12 -0300

    Recriação do projeto

---
+++ desafio-cyberpunk/app/src/main/java/br/com/luisferreira/desafiocyberpunk/activities/CloneActivity.java
@@ -0,0 +1,9 @@
+package br.com.luisferreira.desafiocyberpunk.activities;
+
+/**
+ * Created by Luis Ferreira on 04/03/2018.
+ */
+
+public class CloneActivity {
+    
+}
---
+++ desafio-cyberpunk/app/src/main/java/br/com/luisferreira/desafiocyberpunk/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.luisferreira.desafiocyberpunk.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.luisferreira.desafiocyberpunk.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
