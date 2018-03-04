b0f30987b8d253568b7cdb759d78683ba1a7858d
LucasufbaCC <lucassouza@dcc.ufba.br>
2018-03-04 16:16:59 -0300

    melhoria no script de inicializacao

--- desafioSolutis/start.sh
+++ desafioSolutis/start.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
 
-echo "Parando containers \n"
+echo "Parando containers "
 docker stop $(docker ps -qa)
 
-echo "Parando containers \n"
-docker rm demo-mysql
+echo "Excluído nome da imagem caso exista "
+docker rm db-mysql
 
-echo "Subindo imagem mysql \n"
-docker run --name demo-mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=desafio_solutis -e MYSQL_USER=desafio_solutis -e MYSQL_PASSWORD=desafio -d mysql:5.6
+echo "Subindo imagem mysql "
+docker run --name db-mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=desafio_solutis -e MYSQL_USER=desafio_solutis -e MYSQL_PASSWORD=desafio -d mysql:5.6
 
-echo "Contruindo projeto \n"
+echo "Contruindo projeto "
 mvn clean package docker:build
 
-docker rm demo-app
+echo "Excluído nome da imagem caso exista do app "
+docker rm app
 
-echo "Aplicacao!! \n "
-docker run -p 8080:8080 --name demo-app --link demo-mysql:mysql ehcoo/spring-boot-docker-mysql
+echo " Subindo Aplicacao "
+docker run -p 8080:8080 --name app --link db-mysql:mysql -d ehcoo/spring-boot-docker-mysql
+
+echo "Aplicação no ar \o/ "
+
+port="8080"
+b=$(docker-machine ip)
+echo "docker machine ip = "$b
+
+echo "Rotas do servico:"
+echo "GET    "$b":"$port"/clones "
+echo "GET    "$b":"$port"/clones/{id} "
+echo "PUT    "$b":"$port"/clones/{id} "
+echo "POST   "$b":"$port"/clones/add "
+echo "DELETE "$b":"$port"/clones/{id} "
+
