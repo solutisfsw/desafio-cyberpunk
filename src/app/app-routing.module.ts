5989867f94948eb56b6750cf9211550ebca36671
marciosmelo <marciomelo@gmail.com>
2018-03-04 19:21:53 -0300

    Creating the delete clones route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'add', component: CloneCreateComponent },
   { path: 'detail/:id', component: CloneDetailComponent }
+  
 ];
 
 @NgModule({
