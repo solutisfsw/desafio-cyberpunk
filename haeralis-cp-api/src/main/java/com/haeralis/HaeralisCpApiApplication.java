027c86bfcf2ea0d7ff13680b7dd559663f59d2f0
Haeralis <gledsonivomotta@gmail.com>
2020-02-28 14:50:19 -0300

    Charset adjustment.

--- haeralis-cp-api/src/main/java/com/haeralis/HaeralisCpApiApplication.java
+++ haeralis-cp-api/src/main/java/com/haeralis/HaeralisCpApiApplication.java
@@ -1,7 +1,11 @@
 package com.haeralis;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 /**
  * 
@@ -13,5 +17,17 @@ public class HaeralisCpApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HaeralisCpApiApplication.class, args);
+	}
+	
+	@Bean
+	public Filter getCharacterEncodingFilter() {
+
+	    CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+
+	    encodingFilter.setEncoding("UTF-8");
+	    encodingFilter.setForceEncoding(true);
+
+	    return encodingFilter;
+
 	}
 }
