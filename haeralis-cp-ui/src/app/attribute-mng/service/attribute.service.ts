d4f7af64c67394b743cae004af445235f8d2998f
Gledson Motta <gledsonivomotta@gmail.com>
2020-04-10 15:13:03 -0300

    New changes.

--- haeralis-cp-ui/src/app/attribute-mng/service/attribute.service.ts
+++ haeralis-cp-ui/src/app/attribute-mng/service/attribute.service.ts
@@ -4,25 +4,26 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class AttributeService {
 
-  public attributePath = 'http://localhost:8080/attribute';
+  public attributePath = 'http://localhost:8080/attributes';
+  public attributeTypePath = 'http://localhost:8080/attribute-types';
 
   constructor(private http: HttpClient) { }
 
   listAll(){
-    return this.http.get<any[]>(this.attributePath+'/list');
+    return this.http.get<any[]>(this.attributePath);
   }
 
   listType(){
-    return this.http.get<any[]>(this.attributePath+'/listType');
+    return this.http.get<any[]>(this.attributeTypePath);
   }
 
   addNew(attribute){
-    return this.http.post(this.attributePath+'/add', attribute)
+    return this.http.post(this.attributePath, attribute)
   }
 
   delete(attribute){
     //console.log(attribute);
-    return this.http.post(this.attributePath+'/delete', attribute);
+    return this.http.delete(this.attributePath+"/"+attribute.id);
   }
 
 }
